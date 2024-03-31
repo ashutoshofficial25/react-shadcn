b242e8ecbeca6f91484b0e1f638ef154a81b51d0
Ashutosh Maurya <ashutoshm9695@gmail.com>
2024-03-31 16:49:21 +0530

    h page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex items-center justify-center bg-primary">
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle> React - Tailwind - ShadCn template </CardTitle>
+            <CardDescription>You can use it.</CardDescription>
+          </CardHeader>
+          <CardContent className="text-center text-teal-500">
+            <p>just clone and use</p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
